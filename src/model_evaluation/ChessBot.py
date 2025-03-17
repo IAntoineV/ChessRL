9525e9ddaedacd061e12977b225c450638257a6c
IAntoineV <antoinevialle.ia@gmail.com>
2025-03-17 21:42:54 +0100

    [reward model]

---
+++ src/model_evaluation/ChessBot.py
@@ -0,0 +1,10 @@
+import abc
+
+
+class ChessBot(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def get_bot_move(self, move):
+        pass
+
+    @abc.abstractmethod
+    def initialize_pos(self, list_moves):
